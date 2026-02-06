fe64c3b1d9217d361757d9af4c5e559f91c900ae
Rafaellfckkj <rafaxolive@gmail.com>
2026-02-06 12:20:24 -0300

    aprendendo ciclo de vida de um component

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.html',
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  ngOnInit(): void {
+    this.mensagem();
+  }
 
+  mensagem() {
+    console.log('O meu componente navbar foi iniciado. dentro de uma função');
+  }
 }
