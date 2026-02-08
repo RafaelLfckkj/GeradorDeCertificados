17bdf6ad5fda907f9342bcbb5fc772dfb6e9bd8f
Rafaellfckkj <rafaxolive@gmail.com>
2026-02-08 11:18:04 -0300

    Criação da pagina de Certificado

---
+++ src/app/pages/certificado/certificado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from "../../components/secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certificado',
+  imports: [SecondaryButton],
+  templateUrl: './certificado.html',
+})
+export class Certificado {
+
+}
