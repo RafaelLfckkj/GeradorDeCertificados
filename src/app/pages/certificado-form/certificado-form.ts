884f9c96fe32b9d51de9cc205bec51da0a0c57df
Rafaellfckkj <rafaxolive@gmail.com>
2026-02-07 23:11:22 -0300

    Página de formulário do certificado

---
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PrimaryButton } from "../../components/primary-button/primary-button";
+import { SecondaryButton } from "../../components/secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certificado-form',
+  imports: [PrimaryButton, SecondaryButton],
+  templateUrl: './certificado-form.html',
+})
+export class CertificadoForm {
+
+}
--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../components/secondary-button/secondary-button";
+import { ItemCertificado } from "../../components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, ItemCertificado],
   templateUrl: './certificados.html',
 })
 export class Certificados {
